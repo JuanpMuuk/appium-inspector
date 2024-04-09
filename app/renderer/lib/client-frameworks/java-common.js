9d02c7805ba46d5b3a06012824203fd9b1596ff3
JuanpMuuk <juan.ramos@muuklabs.com>
2024-04-09 11:09:27 -0600

    Updated swipe

--- app/renderer/lib/client-frameworks/java-common.js
+++ app/renderer/lib/client-frameworks/java-common.js
@@ -124,19 +124,7 @@ class JavaFramework extends Framework {
   //TODO adapt return for our code
   codeFor_swipe(varNameIgnore, varIndexIgnore, pointerActions) {
     const {x1, y1, x2, y2} = this.getSwipeCoordinatesFromPointerActions(pointerActions);
-    return `
-final var finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
-var start = new Point(${x1}, ${y1});
-var end = new Point (${x2}, ${y2});
-var swipe = new Sequence(finger, 1);
-swipe.addAction(finger.createPointerMove(Duration.ofMillis(0),
-    PointerInput.Origin.viewport(), start.getX(), start.getY()));
-swipe.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
-swipe.addAction(finger.createPointerMove(Duration.ofMillis(1000),
-    PointerInput.Origin.viewport(), end.getX(), end.getY()));
-swipe.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
-driver.perform(Arrays.asList(swipe));
-  `;
+    return `Actions.swipeOnCoordinates(driver, ${x1}, ${y1}, ${x2}, ${y2});`;
   }
 
   // Execute Script
